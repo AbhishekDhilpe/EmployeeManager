9e3e92805f7ae71bb9806fbe47185738095574a2
Abhishek Dhilpe <abhishek.m.dhilpe@gmail.com>
2021-07-07 10:31:04 +0530

    first

---
+++ Manager.java
@@ -0,0 +1,8 @@
+package com.day01assignment.java;
+
+public class Manager {
+	public static void main(String[] args) {
+		Employee e1= new Employee(101,"Abhishek");
+		System.out.println(e1.toString());
+	}
+}
